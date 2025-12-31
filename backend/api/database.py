825ec97cff3268b37d433ecb0ad69e101d27fdc2
Aishwarya <143599092+aishulle@users.noreply.github.com>
2025-12-31 09:18:05 +0530

    Update database.py

--- backend/api/database.py
+++ backend/api/database.py
@@ -6,7 +6,7 @@ DATABASE_URL = "sqlite:///my_app/PDFDataBase.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-# ✅ Just define get_db here, no need to import SessionLocal
+
 def get_db():
     db = SessionLocal()
     try:
